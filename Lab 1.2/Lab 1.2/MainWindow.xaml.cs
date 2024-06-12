346cd702464dc6ce640d3b877552a0683e3c8dc1
Vlad-22-C <vladislavleskov435@gmail.com>
2024-06-12 20:30:43 +0700

    Add files via upload

---
+++ Lab 1.2/Lab 1.2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_1._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textToAdd = TextField.Text;
+
+            if (!string.IsNullOrEmpty(textToAdd))
+            {
+                TextList.Items.Add(textToAdd);
+                TextField.Text = "";
+            }
+        }
+    }
+}
